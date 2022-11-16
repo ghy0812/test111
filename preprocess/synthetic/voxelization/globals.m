64d18f43d1ec3bf133ea4c5b6365bb4e79310c30
ghy0812 <66369212+ghy0812@users.noreply.github.com>
2022-11-16 09:51:04 +0800

    Delete preprocess directory

--- preprocess/synthetic/voxelization/globals.m
+++
@@ -1,3 +0,0 @@
-global cachedir
-global basedir
-global shapenetDir
--- preprocess/synthetic/voxelization/startup.m
+++
@@ -1,8 +0,0 @@
-global cachedir
-global basedir
-global shapenetDir
-
-%% Datasets
-basedir = pwd();
-cachedir = fullfile(basedir,'..','..','..','cachedir');
-shapenetDir = '/data1/shubhtuls/cachedir/Datasets/shapeNetCoreV1/';
